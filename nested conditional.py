dcb8e05724fd7ff006edfc5e75b058eb50baa0f9
Fabliha Afaf Sarwar <117925886+fabliha13@users.noreply.github.com>
2025-01-03 22:53:16 +0600

    class 3

--- nested conditional.py
+++ nested conditional.py
@@ -1,17 +1,32 @@
-#bus fare problem from Mirpur to Banani
+###bus fare problem from Mirpur to Banani
+
+# passenger_type = input("Are you a student? Y/N:").lower()
+# fare = 50
+
+# if passenger_type == "y":
+#     age = int(input("What's your age?"))
+#     if age<=18:
+#         new_fare = fare/2
+#         print(f"Your fare is {new_fare}")
+#     else:
+#         new_fare = (fare/2) + 5
+#         print(f'Yor fare is {new_fare}')
+
+# else:
+#     print(f'Your fare is {fare}')
 
 
-passenger_type = input("Are you a student? Y/N:").lower()
-fare = 50
+## Classwork: Grading system Problem
 
-if passenger_type == "y":
-    age = int(input("What's your age?"))
-    if age<=18:
-        new_fare = fare/2
-        print(f"Your fare is {new_fare}")
-    else:
-        new_fare = (fare/2) + 5
-        print(f'Yor fare is {new_fare}')
+marks = 56
 
-else:
-    print(f'Your fare is {fare}')
+if marks>50 and marks<=60:
+    print("you get a D")
+    if marks>55:
+        print("You also se get a star")
+
+elif marks>60 and marks<=70:
+    print("You get a C")
+    if marks>65:
+        print("You also se get a star")
+
