311bc068d706750ffe603b28533bd3355bec018b
Fabliha Afaf Sarwar <117925886+fabliha13@users.noreply.github.com>
2025-01-04 16:21:05 +0600

    Class 4

---
+++ for.py
@@ -0,0 +1,30 @@
+## print numbers from 1 to 10 using for loop
+
+# for number in range(1, 10 + 1, 1): #add 1 with the ending point
+#     print(number)
+
+# print numvers from 10 to 1 using for loop
+
+# for number in range(10, 0, -1):
+#     print(number)
+
+## print even numbers between 1 to 10 using for loop
+
+# for number in range(2, 11, 2):
+#     print(number)
+
+# for number in range(10):
+#     print(number)
+
+s = "Saturday"
+
+# for ch in s:
+#     print(ch)
+
+# for i in range(len(s)): #range(0,8)
+#     print(s[i])
+
+my_list = ["pens", "books", "eggs"]
+
+for item in my_list:
+    print(item)
---
+++ while.py
@@ -0,0 +1,30 @@
+## forward moving: print all numbers from 1 to 10
+
+# i = 1
+# while i<=10:
+#     print(i)
+#     i = i+1
+
+## backward moving: print all numbers from 10 to 1
+
+# i = 10
+# while i>=1:
+#     print(i)
+#     i = i - 1
+
+##print only the even numbers from 1 to 10
+
+# i = 0
+
+# while i <= 10:
+
+#     print(i)
+
+#     i = i + 2
+
+i = 1
+while (i<=10):
+    if i % 2 == 0:
+        print(i)
+
+    i += 1
