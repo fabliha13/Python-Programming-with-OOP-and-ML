790fb874f32c4364d5a307055caf3a37a8b91c30
Fabliha Afaf Sarwar <117925886+fabliha13@users.noreply.github.com>
2025-01-23 20:50:44 +0600

    class 5

---
+++ list.py
@@ -0,0 +1,46 @@
+my_list = ["eggs", "fish", "pencils"]
+
+# #add an item
+# my_list.append("books")
+# print(my_list)
+
+# #last element remove
+# my_list.pop()
+# print(my_list)
+
+# #list indexing
+# print(my_list[1])
+# print(my_list[-1])
+
+# #insering an element
+# my_list.insert(1,"tomatoes")
+# print(my_list)
+
+# #removing an item
+# my_list.remove("fish")
+# print(my_list)
+
+# # removing item from a specific index
+# del my_list[1]
+# print(my_list)
+
+# #index of an item
+# idx_pencil = my_list.index("pencils")
+# print(idx_pencil)
+
+# #number of items
+# len_of_list = len(my_list)
+# print(len_of_list)
+
+# # #counting number of specific item
+# students = ["arman", "tahiya", "sumera", "arman"]
+# count_of_arman = students.count("arman")
+## print(count_of_arman)
+
+#printing the items of a list
+# for item in students:
+#     print(item)
+
+numbers = [1, 2, 3, 4, 5, 6]
+for item in numbers:
+    print(item ** 2)
